788361c670781c7661ae57deac7bde6b3e441189
Aciel <106684053+Aciel07@users.noreply.github.com>
2024-03-12 22:58:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,17 @@ chatbot = EnSysBot("gpt-4")
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/chat_1')
+def chat_1():
+    return render_template('chat_1.html')
+
+@app.route('/chat_2')
+def chat_2():
+    return render_template('chat_2.html')
+
+@app.route('/chat_3')
+def chat_3():
+    return render_template('chat_3.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
